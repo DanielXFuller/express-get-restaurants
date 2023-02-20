f5e441cfb6c9f4cf44587a9b18c5e4fa4a8c8773
DanielXFuller <danielxfuller@gmail.com>
2023-02-20 11:08:09 +0000

    finished task

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ const {sequelize} = require("./db");
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
-
+app.get('/restaurants', async (req, res) => {
+    try {
+      const restaurants = await Restaurant.findAll();
+      res.json(restaurants);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server error');
+    }
+});
 
 app.listen(port, () => {
     sequelize.sync();
